322847164405e92e2c4750621543b5c4194c4006
Jens Rehsack <sno@netbsd.org>
2021-01-27 11:32:14 +0100

    staging: fsl-dpaa2/mac: mark configurable function __maybe_unused
    To avoid errors during hardened CFLAGS with commit
        bb562e6e0358 kbuild: enforce -Werror=return-type
    optional entry points are marked as `__maybe_unused`.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- drivers/staging/fsl-dpaa2/mac/mac.c
+++ drivers/staging/fsl-dpaa2/mac/mac.c
@@ -218,7 +218,7 @@ done:
 #include <linux/phy.h>
 
 #ifdef CONFIG_FSL_DPAA2_MAC_USERSPACE_ACCESS
-static int dpaa2_mac_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
+static int __maybe_unused dpaa2_mac_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 {
 	struct mii_ioctl_data *data = if_mii(ifr);
 
